d97b8ff2e1d6a8ec3368598360a1dd31b23b9450
trusman_chen <357063127@qq.com>
2017-01-22 17:25:57 +0800

    更新请假详情页

--- resources/js/flow/leftdetails.js
+++ resources/js/flow/leftdetails.js
@@ -16,6 +16,9 @@ define(['vue', 'css!iconfont', 'css!commoncss', 'css!leftdetailsCSS'], function(
 
             var flowState = that.renderFlowState('flowState');
             this.reqFlowStateData('../../resources/json/flowState.json', flowState);
+
+            that.handleAllowClick();
+            that.handleRefuseClick();
         };
 
         this.reqFlowFormData = function(url, vm) {
@@ -151,6 +154,66 @@ define(['vue', 'css!iconfont', 'css!commoncss', 'css!leftdetailsCSS'], function(
                 history.back();
             });
         };
+
+        this.handleAllowClick = function() {
+            $('#flowAllow').on('click', function(e) {
+                $.modal({
+                    text: '<span class="iconfont icon-dui color-limegreen icon-big p-r-sm"></span>批准完成！',
+                    afterText: '<p class="app-modal-remarks"><span id="backCount" class="color-red">2</span> 秒后跳转【我的待办】</p>'
+                });
+                var timer = setInterval(function() {
+                    var count = $('#backCount').text();
+                    if (count > 0) $('#backCount').text(--count)
+                    if (count === 0) {
+                        clearInterval(timer);
+                        $.closeModal();
+                        history.back();
+                    }
+                }, 1000);
+            });
+        };
+
+        this.handleRefuseClick = function() {
+            $('#flowRefuse').on('click', function(e) {
+                $.modal({
+                    text: '<div class=" m-t-lg"><span class="color-red">驳回</span> 此请假申请！</div>',
+                    afterText: '\
+                        <span class="app-flow-refuse-close iconfont icon-cha"></span>\
+                        <div class="app-flow-refuse list-block m-t-xs m-b-none">\
+                            <ul class="b-t-none b-b-none">\
+                                <li>\
+                                    <div class="item-content">\
+                                    <div class="item-inner">\
+                                        <div class="item-input item-caret">\
+                                            <select>\
+                                                <option>申请人节点</option>\
+                                                <option>部分负责人节点</option>\
+                                                <option>直接上司节点</option>\
+                                                <option>分管领导节点</option>\
+                                            </select>\
+                                        </div>\
+                                    </div>\
+                                    </div>\
+                                </li>\
+                                <li>\
+                                    <div class="item-content">\
+                                    <div class="item-inner">\
+                                        <div class="item-input">\
+                                            <input type="text" placeholder="写下你驳回的意见">\
+                                        </div>\
+                                    </div>\
+                                    </div>\
+                                </li>\
+                            </ul>\
+                        </div>\
+                        <p><a id="refuseConfirm" href="javascript:;" class="button bg-blue color-white">确认</a></p>\
+                    '
+                });
+                $('.app-flow-refuse-close').click(function() {
+                    $.closeModal();
+                });
+            });
+        }
     }
     return new page();
 });
